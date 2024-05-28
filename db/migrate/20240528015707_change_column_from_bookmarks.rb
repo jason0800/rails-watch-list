f7c921ba28715137b76817254a6ef31eb2b2db45
Jason Wang <jasonwang332@gmail.com>
2024-05-28 12:43:50 +1000

    validations and associations done

---
+++ db/migrate/20240528015707_change_column_from_bookmarks.rb
@@ -0,0 +1,6 @@
+class ChangeColumnFromBookmarks < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :bookmarks, :movie_id, :integer
+    remove_column :bookmarks, :list_id, :integer
+  end
+end
